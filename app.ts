ff6ee7586651bfb7441dfea1253025a6f0e2ed7a
parthjeeves <parth.chauhan@tryjeeves.com>
2021-10-06 15:36:14 +0530

    Deno demo

---
+++ app.ts
@@ -0,0 +1,19 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import {
+  viewEngine,
+  engineFactory,
+  adapterFactory,
+} from "https://deno.land/x/view_engine/mod.ts";
+
+const denjuckEngine = await engineFactory.getDenjuckEngine();
+const oakAdapter = await adapterFactory.getOakAdapter();
+
+const app = new Application();
+
+app.use(viewEngine(oakAdapter, denjuckEngine));
+
+app.use(async (ctx, next) => {
+  ctx.render("index.html", { data: { name: "John" } });
+});
+
+await app.listen({ port: 8000 });
---
+++ index.ts
@@ -0,0 +1,12 @@
+// import { serve } from "https://deno.land/std/http/server.ts";
+// let message : string;
+// const server=serve({port:8000});
+
+// message="Het i am parth 🦕";
+// console.log(message);
+
+// for await (const req of server) {
+//     req.respond({body:"HeY Welcome to Deno!"});
+// }
+
+console.log(Deno);
